1dd02f4b7c2d8e208a52cca1729796e6c5e8a3bd
Alain Pimentel Carpio <icedragonceer.tv@gmail.com>
2026-06-18 22:04:59 -0600

    Físicas de salto y portería funcionando

--- Assets/Scrips/GoalZone.cs
+++ Assets/Scrips/GoalZone.cs
@@ -4,7 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Balon"))
         {
             Debug.Log("¡Anotación!");
             // Aquí puedes agregar lógica adicional, como cargar la siguiente escena o mostrar un mensaje de victoria.
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Curso basico de Unity
 -
 - Mi primer mundo en Unity y control de versiones
 - El Cerebro del Jugador (C#, Cámara y Física Inicial)
+- Leyes de la Física, Colisiones y Sensores
